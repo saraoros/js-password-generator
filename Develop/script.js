b8baaa50fa0b75cb094c1c7261c0d4fe3f6c3533
Sara Gochez <saragochez13@gmail.com>
2022-01-13 11:17:48 -0800

    modified functions and added new vars

--- Develop/script.js
+++ Develop/script.js
@@ -1,84 +1,115 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-window.alert("Welcome to our password generator! We're excited to help you create a safe password. Please answer the following prompts to help us create the perfect password for you.") 
+window.alert(
+  "Welcome to our password generator! We're excited to help you create a safe password. Please answer the following prompts to help us create the perfect password for you."
+);
 
- var getLength = window.prompt("How long would you like your password to be? Choose a number between 8-128 and type the desired length in the box below.");
-var getUpperCase = window.prompt("Would you like your password to contain upper case letters? Type 'YES' or 'NO' in the box below.");
+var getLength = window.prompt(
+  "How long would you like your password to be? Choose a number between 8-128 and type the desired length in the box below."
+);
 
- var getLowerCase = window.prompt("Would you like your password to contain lower case letters? Type 'YES' or 'NO' in the box below.");
+var getUpperCase = window.prompt(
+  "Would you like your password to contain upper case letters? Type 'YES' or 'NO' in the box below."
+);
 
- var getSymbols = window.prompt("Would you like your password to contain special characters? Type 'YES' or 'NO' in the box below.");
+var getLowerCase = window.prompt(
+  "Would you like your password to contain lower case letters? Type 'YES' or 'NO' in the box below."
+);
 
- var getNumbers = window.prompt("Would you like your password to contain numbers? Type 'YES' or 'NO' in the box below.");
+var getSymbols = window.prompt(
+  "Would you like your password to contain special characters? Type 'YES' or 'NO' in the box below."
+);
+
+var getNumbers = window.prompt(
+  "Would you like your password to contain numbers? Type 'YES' or 'NO' in the box below."
+);
+
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+
+var numberChar = "0123456789";
+
+var symbols = "!@#$%^&*(){}[]=<>/,.";
+
+var allChars = "";
+
+console.log(allChars);
+
+console.log(getLength, getUpperCase, getLowerCase, getSymbols, getNumbers);
+
+if (getLength < 8 || getLength > 128 || !getLength || isNaN(getLength)) {
+  alert("Invalid entry");
+} else {
+  console.log("Valid entry");
+  getUpperCaseFn();
+  getLowerCaseFn();
+  getSymbolsFn();
+  getNumbersFn();
+  console.log(allChars);
+  // I added this last part in hopes of it working
+  generatePassword();
+}
+
+for (var i = 0; i < getLength; i++) {
+  //math floor math random, mutiply to all Chars
+  //return String.fromCharCode(Math.floor(Math.random() * 26) + 65); 'illegal return statement'
+  //return Math.floor.random() * allChars; 'illegal return statement'
+  //return String.fromCharCode(Math.floor(Math.random() * 26) + 97); 'illegal return statement'
 
 
-// Start of If statements & functions
+  //If math random cannot be applied to a string, then turn that string to array by using a split method
+}
 
 
-/* if (getUpperCase === "NO" || getUpperCase === "no"); { */
-   // Not sure if I should make a statement for the 'no' option
-function getUpperCase() {
-if (getUpperCase === "YES" || getUpperCase === "yes");
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+// Start of functions
+function getUpperCaseFn() {
+  if (getUpperCase.toUpperCase() === "YES") {
+    allChars += upperCase;
   }
- 
-/* if (getLowerCase === "NO" || getLowerCase === "no"); { */
-  // Not sure if I should make a statement for the 'no' option
+}
 
-function getLowerCase() {
-if  (getLowerCase === "YES" || getLowerCase === "yes"); 
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+function getLowerCaseFn() {
+  if (getLowerCase.toUpperCase === "YES") {
+    allChars += lowerCase;
   }
- 
-
-/* if (getSymbols === "NO" || getSymbols === "no"); { */
-  // Not sure if I should make a statement for the 'no' option
-
-function getSymbols() {
-if (getSymbols === "YES" || getSymbols === "yes");
-	const symbols = '!@#$%^&*(){}[]=<>/,.'
-	return symbols[Math.floor(Math.random() * symbols.length)];
+  //return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
-
-/* if (getNumbers === "NO" || getNumbers === "no"); { */
-  // Not sure if I should make a statement for the 'no' option
-
-function getNumbers() {
-if (getNumbers === "YES" || getNumbers === "yes");
-	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+function getSymbolsFn() {
+  if (getSymbols === "YES" || getSymbols === "yes") {
+    allChars += symbols;
+  }
+  //return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
-
-// I know I need a for loop but I am not sure how to add it or what to put in it exactly.
-function getLength() {
-
+function getNumbersFn() {
+  if (getNumbers === "YES" || getNumbers === "yes") {
+    allChars += numberChar;
+  }
+  //return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
+function getLengthFn() {}
 
-
+//bottom of JS?
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
 
 function generatePassword() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-
 }
-
-
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); {
-
+generateBtn.addEventListener("click", writePassword);{
 }
+
+// End of bottom JS??
